dd08a6d3500ed754a2b7452a2c09d74ade278756
Daniil <azbukin2003@gmail.com>
2024-09-27 20:22:14 +0300

    Made classes using pydantic

--- practice_2/main.py
+++ practice_2/main.py
@@ -1,22 +1,55 @@
-"""
-    Практическая работа №2
-    АЗБУКИН ДАНИИЛ ЮРЬЕВИЧ БСБО-10-21
-"""
-
-class Ingredient:
-
-    def __init__(self, name, raw_weight, weight, amount, cost):
-        self.name = name
-        self.raw_weight = raw_weight
-        self.weight = weight
-        self.amount = amount
-        self.cost = cost
+from pydantic import BaseModel, PositiveInt
+from typing import List
 
 
-class Recipe:
+class Ingredient(BaseModel):
+    name: str
+    raw_weight: PositiveInt
+    weight: PositiveInt
+    amount: PositiveInt
+    cost: PositiveInt
+
+
+class Recipe(BaseModel):
+    name: str
+    ingredients: List[Ingredient]
 
     def __init__(self, name, ingredients):
-        self.ingredients = [Ingredient(name=name, raw_weight=raw_weight, weight=weight, amount=amount, cost=cost)
-                            for name, raw_weight, weight, amount, cost in ingredients]
-        self.name = name
+        new_ingredients = [Ingredient(name=name, raw_weight=raw_weight, weight=weight, amount=amount, cost=cost)
+                           for name, raw_weight, weight, amount, cost in ingredients]
+        super().__init__(name=name, ingredients=new_ingredients)
 
+    def calc_cost(self, portions=1):
+        total_cost = sum(item.cost * item.amount for item in self.ingredients)
+        return total_cost * portions
+
+    def calc_weight(self, portions=1, raw=False):
+        if raw:
+            return sum(item.raw_weight * item.amount for item in self.ingredients) * portions
+
+        return sum(item.weight * item.amount for item in self.ingredients) * portions
+
+    def __str__(self):
+        return self.name
+
+
+if __name__ == '__main__':
+
+    receipt_from_api = {
+        'title': 'Азу',
+        'ingredients_list': [
+            ('Говядина', 150, 125, 2, 300),
+            ('Картофель', 80, 60, 3, 50),
+            ('Лук репчатый', 80, 60, 1, 30),
+            ('Помидоры', 150, 100, 1, 70),
+            ('Масло сливочное', 150, 50, 1, 90),
+            ('Огурцы', 70, 60, 3, 20)
+        ]
+    }
+
+    receipt = Recipe(receipt_from_api['title'], receipt_from_api['ingredients_list'])
+
+    print(f'Рецепт: {str(receipt)}')
+    print(f"Стоимость 1 порции: {receipt.calc_cost()}")
+    print(f"Вес 1 порции: {receipt.calc_weight()}")
+    print(f"Сырой вес 1 порции: {receipt.calc_weight(raw=True)}")
